367e22a4f8a84413e9a4dd01c0788cb716554d37
TigerWest <sj441888@naver.com>
2016-08-26 11:45:25 +0900

    우편번호 추가
    이전 수정은 id체크

---
+++ WebContent/js/script.js
@@ -0,0 +1,16 @@
+function zipCheck(){
+	url="zipcheck.jsp?check=y";
+	window.open(url,"zip","toolbar=no,width=350,heigth=300,top=200,left=300,status=yes,scrollbars=yes,menubar=no");
+}
+function idCheck(){
+	if(regForm.id.value=""){
+		alert("id를 입력하시오");
+		regForm.id.focus();
+	}else{
+		url="idcheck.jsp?id="+regForm.id.value;
+		window.open(url,"id","width=300,height=150,top=150,left=150");
+	}
+}
+function inputCheck(){
+	
+}
---
+++ src/shop/member/MemberMgr.java
@@ -0,0 +1,67 @@
+package shop.member;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class MemberMgr {
+
+	private Connection con;
+	private PreparedStatement pst;
+	private ResultSet rs;
+	private DataSource ds;
+	
+	
+	public MemberMgr() {
+		try {
+			Context context= new InitialContext();
+			ds=(DataSource)context.lookup("java:comp/env/jdbc_maria");
+		} catch (Exception e) {
+			System.out.println("db 연결실패"+e);
+		}
+	}
+	//우편번호 검색
+	public ArrayList<ZiptabBean> zipcodeRead(String area3){
+		ArrayList<ZiptabBean> list =new ArrayList<>();
+		String sql="select * from ziptab where area3 like ?";
+		try {
+			con=ds.getConnection();
+			pst=con.prepareStatement(sql);
+			pst.setString(1, area3+"%");
+			rs=pst.executeQuery();
+			rs.next();
+			while(rs.next()){
+				ZiptabBean bean= new ZiptabBean();
+				bean.setZipcode(rs.getString("zipcode"));
+				bean.setArea1(rs.getString("area1"));
+				bean.setArea2(rs.getString("area2"));
+				bean.setArea3(rs.getString("area3"));
+				bean.setArea4(rs.getString("area4"));
+				list.add(bean);
+			}
+			
+			
+		} catch (Exception e) {
+			System.out.println("zipcodeRead err"+e);
+		}finally{
+			try {
+				if(rs!=null)rs.close();
+				if(pst!=null)pst.close();
+				if(con!=null)con.close();
+			} catch (Exception e2) {
+			}
+		}
+		return list;
+		
+	}
+	public boolean checkId(String id){
+		boolean b=false;
+		return b;
+	}
+
+}
