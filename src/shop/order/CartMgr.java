a7baf1157d5a9e805d0291122543d213b5a9102e
TigerWest <sj441888@naver.com>
2016-08-30 11:10:27 +0900

    주문관리페이지 추가
    장바구니관리와 주문관리를 위한
    자바 파일 추가

---
+++ src/shop/order/CartMgr.java
@@ -0,0 +1,19 @@
+package shop.order;
+
+import java.util.Hashtable;
+
+public class CartMgr {
+	private Hashtable hCart=new Hashtable<>();
+	public void addCart(OrderBean bean){
+		
+	}
+	public Hashtable getCartList(){
+		return hCart;
+	}
+	public void updateCart(OrderBean obean){
+		
+	}
+	public void deleteCart(OrderBean obean){
+		
+	}
+}
